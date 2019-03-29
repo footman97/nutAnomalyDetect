a54932ccaf5151e47cc81e429055aa384bb28ca1
liujun <pikagou@gmail.com>
2019-03-29 12:20:52 +0800

    添加readme

---
+++ target/classes/resources/uploadFile.js
@@ -0,0 +1,50 @@
+// 加载完成后执行
+$(document).ready(function(){
+
+    // method2
+    $("#input-704").fileinput({
+        language: 'zh', //设置语言
+        allowedFileExtensions: ['xlsx', 'xls'],  //接收的文件后缀
+        uploadUrl:  "/upload",  //上传的地址
+        minFileCount: 1,
+        maxFileCount: 5,
+        uploadAsync: true, //默认异步上传
+        showUpload: true, //是否显示上传按钮
+
+        showRemove : true, //显示移除按钮
+        showPreview : true, //是否显示预览
+
+    });
+
+    //异步上传返回结果处理
+    $("#input-704").on("fileuploaded", function (event, data, previewId, index) {
+        var response = data.response;
+        var prompt = "Success! file path: "  + response.path;
+        alert(prompt);
+
+    });
+
+});
+
+
+// method1 without bootstrap-inputfile
+$("#ud").click(function(){
+
+    alert("submit begin!");
+
+    var type = "file";          //后台接收时需要的参数名称，自定义即可
+    var id = "cert";            //即input的id，用来寻找值
+    var formData = new FormData();
+    formData.append(type, $("#"+id)[0].files[0]);    //生成一对表单属性
+    $.ajax({
+        type: "POST",           //因为是传输文件，所以必须是post
+        url: '/upload',         //对应的后台处理类的地址
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (data) {
+            alert(data);
+        }
+    });
+});
+
